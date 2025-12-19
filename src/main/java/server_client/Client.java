0e78a06d1afcbb4531f2a161d240a257be04936a
Moritz Binder <ic24b002@technikum-wien.at>
2025-12-19 08:51:25 +0100

    Fixed some Warnings

--- src/main/java/server_client/Client.java
+++ src/main/java/server_client/Client.java
@@ -1,14 +1,9 @@
 package server_client;
 
 import fhtw.accuweatherapp.UI;
-import javafx.application.Application;
 
-import java.io.IOException;
-import java.net.Socket;
 
 public class Client {
-    private static final String HOST = "localhost";
-    private static final int SERVER_PORT = 8080;
 
     public static void main(String[] args) {
         System.out.println("--- Starting AccuWeather Client ---");
