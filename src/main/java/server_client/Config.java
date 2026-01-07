df689ade5bed957cc265be995d14a15d1c16ce83
Moritz Binder <ic24b002@technikum-wien.at>
2026-01-07 16:26:39 +0100

    Updatet API settings

--- src/main/java/server_client/Config.java
+++ src/main/java/server_client/Config.java
@@ -6,7 +6,7 @@ import io.github.cdimascio.dotenv.Dotenv;
  * Einfache Klasse zum Laden der Konfiguration aus der .env Datei
  */
 public class Config {
-    private static final Dotenv dotenv;
+   /* private static final Dotenv dotenv;
 
     static {
         // Lade die .env Datei aus dem Projekt-Root
@@ -14,13 +14,13 @@ public class Config {
                 .directory(".")
                 .ignoreIfMissing()
                 .load();
-    }
+    }*/
 
     /**
      * Gibt den OpenWeatherMap API Key zurück
      */
     public static String getApiKey() {
-        String apiKey = dotenv.get("OPENWEATHER_API_KEY");
+        String apiKey = System.getenv("OPENWEATHER_API_KEY");
         if (apiKey == null || apiKey.isEmpty()) {
             throw new RuntimeException("API Key not found! Inspect .env file.");
         }
